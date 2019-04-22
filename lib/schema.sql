85ae7f9cf3db24d1ec120c713a5e815953a64ef9
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 19:27:17 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,3 +14,19 @@ CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT
   );
+  
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+  );
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  MOTTO TEXT,
+  series_id INTEGER,
+  author_id INTEGER
+  );
