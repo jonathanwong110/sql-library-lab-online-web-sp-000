09a18afee1d56eee589cdd8e537aae5803689500
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 21:57:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, year
   FROM books
-  JOIN series
-  ON books.id = series.id
   WHERE series = 1
   ORDER BY year;"
 end
