c66e7ac106e80c77a6453d5cc2988c7bfd26c580
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 22:25:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, id
+  "SELECT authors.name, id
   FROM authors
   JOIN subgenres
   ON authors.id = subgenres.id;"
