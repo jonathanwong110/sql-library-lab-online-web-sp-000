c0493fc01a293b4c5e1d9001b5df3399550c7743
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 21:56:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
   FROM books
   JOIN series
   ON books.id = series.id
-  WHERE series = 1;"
+  WHERE series = 1
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
