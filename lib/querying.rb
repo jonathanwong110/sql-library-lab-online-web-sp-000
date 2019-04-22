f6554a02118f038a66653caabc9b5b13cd4b3ca2
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 21:58:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,14 +6,15 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "select name, motto
+  "SELECT name, motto
   FROM characters
   ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT
+  ;"
 end
 
 def select_name_and_series_subgenres_of_authors
