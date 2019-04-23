06f8b49c3cba0891b0e5e4859ad05f75c7497380
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-23 01:08:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,9 +39,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, COUNT(name)
+  "SELECT characters.name, COUNT(name)
   FROM character_books
-  JOIN characters
-  ON character_id = character_book.characters_id
-  ;"
+  INNER JOIN characters
+  ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(name) DESC;"
 end
