ea901cf72c841b91cf751cc6b117d48ea189d84f
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-23 02:08:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,11 +21,11 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors
-  JOIN series
-  ON authors.id = series.id
-  JOIN subgenres
-  ON series.id = subgenre_id;"
+  FROM series
+  INNER JOIN authors
+  ON series.author_id = authors.id
+  INNER JOIN subgenres
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
