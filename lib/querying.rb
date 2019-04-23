194113b5be06c07fee1edc5184e24d6ec227771b
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-23 01:09:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,8 @@ def select_series_title_with_most_human_characters
   ON characters.series_id = series.id
   GROUP BY Series.title, species
   HAVING characters.species = 'human'
-  ORDER BY COUNT(species) DESC LIMIT 1;"
+  ORDER BY COUNT(species) 
+  DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
