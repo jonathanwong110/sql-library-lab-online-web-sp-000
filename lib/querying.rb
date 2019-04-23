f31329cbafab496206f0e9ffc86dd2e4a3167481
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-23 01:08:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,6 +42,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT name, COUNT(name)
   FROM character_books
   JOIN characters
-  ON character_id = characters_book.characters_id
+  ON character_id = character_book.characters_id
   ;"
 end
